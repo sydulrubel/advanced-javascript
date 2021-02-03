76e5488665a481392e7a79626959c2589cc84097
sydurubel <sydulrubel@gmail.com>
2021-02-03 11:16:15 +0600

    double vs  tripple equal

---
+++ double-vs-tripple-equal.js
@@ -0,0 +1,5 @@
+// defer between double vs triple equal
+
+=={ doble equal means compare two value not type};
+=== { triple equal means check value and type like numbs string boolean then compare it };
+Its preferable for new developer for avoiding error;
