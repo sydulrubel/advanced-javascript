46425b42e3fbac9ee9faf606c879e2724e005ba2
sydurubel <sydulrubel@gmail.com>
2021-02-03 10:57:21 +0600

    defer null vs undefined

---
+++ null-vs-undefine.js
@@ -0,0 +1,8 @@
+// null = null means blank;{A developer have to set null when the value was but not now.};
+
+undefined
+we can get undefined for some reason like
+valu dose not set 
+function not returned but needed;
+need parameter but not provide;
+and so on;
