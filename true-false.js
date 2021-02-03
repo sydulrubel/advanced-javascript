514c2fccc42502155b6a1060fdc05b670f646c66
sydurubel <sydulrubel@gmail.com>
2021-02-03 10:36:48 +0600

    true or false in js

---
+++ true-false.js
@@ -0,0 +1,8 @@
+// when javascript define true or false
+0 = false; {Any number even minus number will be true without 0};
+"" = false; {empty string is false};
+(const defn = false) = false; { If any variabe value false. it will be false};
+null = false;
+undefined = false;{ If any variable undefine then it will be false};
+
+// without upper case javscript detect true 
