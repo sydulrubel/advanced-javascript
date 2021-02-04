45bc378a28c4d5ff83221f421cc3d2832b45dc2d
sydurubel <sydulrubel@gmail.com>
2021-02-04 15:37:31 +0600

    array slice splice and join

---
+++ slice-splice.js
@@ -0,0 +1,11 @@
+child = ['Tahia', 'Munia', 'Sara', 'Saif', 'Rafsan', 'Sadat',  'Saad'];
+// const bari = child.slice(2,7);
+// console.log(bari);
+
+// const blood = child.splice(2,3, 'raisa', "haoa");
+// console.log(blood);
+// console.log(child);
+
+const add = child.join(' moni, ')
+console.log(add)
+
